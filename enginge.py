bf4da1ec034b03aecca9a3a81bb74e5a02693040
MichaelGibsonTheWizard <MichaelGibsonProfessional@gmail.com>
2018-07-07 03:03:13 -0500

    Added basic entities and entity collision

--- enginge.py
+++ enginge.py
@@ -1,7 +1,8 @@
 import libtcodpy as lcod
+from gamestates import GameStates
 from fov_functions import initialize_fov, recompute_fov
 from input_handlers import handle_keys
-from entity import Entity
+from entity import Entity, get_blocking_entities_at_location
 from render_functions import render_all, clear_all
 from map_objects.game_map import GameMap
 
@@ -18,6 +19,8 @@ def main():
     fov_light_walls = True
     fov_radius = 10
 
+    max_monsters_per_room = 3
+
     colors = {
         "dark_wall": lcod.Color(0, 0, 100),
         "dark_ground": lcod.Color(50, 50, 150),
@@ -25,24 +28,25 @@ def main():
         "light_ground": lcod.Color(200, 180, 50),
     }
 
-    player = Entity(int(screen_width/2), int(screen_height/2), '@', lcod.white)
-    npc = Entity(int(screen_width/2 -5), int(screen_height/2), '@', lcod.yellow)
-    entities = [npc, player]
+    player = Entity(0, 0, '@', lcod.white, "Player", blocks=True)
+    entities = [player]
 
     lcod.console_set_custom_font("arial10x10.png", lcod.FONT_TYPE_GRAYSCALE | lcod.FONT_LAYOUT_TCOD)
 
-    lcod.console_init_root(screen_width, screen_height, 'TicTacRogue', False)
+    lcod.console_init_root(screen_width, screen_height, "TicTacRogue", False)
 
     con = lcod.console_new(screen_width, screen_height)
 
     game_map = GameMap(map_width, map_height)
-    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room)
 
     fov_recompute = True
     fov_map = initialize_fov(game_map)
 
     key = lcod.Key()
     mouse = lcod.Mouse()
+
+    game_state = GameStates.PLAYER_TURN
 
     while not lcod.console_is_window_closed():
         lcod.sys_check_for_event(lcod.EVENT_KEY_PRESS, key, mouse)
@@ -62,17 +66,35 @@ def main():
         exit = action.get("exit")
         fullscreen = action.get("fullscreen")
 
-        if move:
+        if move and game_state == GameStates.PLAYER_TURN:
             dx, dy = move
-            if not game_map.is_blocked(player.x + dx, player.y + dy):
-                player.move(dx, dy)
-                fov_recompute = True
+            destination_x = player.x + dx
+            destination_y = player.y + dy
+
+            if not game_map.is_blocked(destination_x, destination_y):
+                target = get_blocking_entities_at_location(entities, destination_x, destination_y)
+
+                if target:
+                    print("You kicked the " + target.name + " in the \'ole spam and eggs")
+                else:
+                    player.move(dx, dy)
+                    fov_recompute = True
+
+                game_state = GameStates.ENEMY_TURN
 
         if exit:
             return True
 
         if fullscreen:
             lcod.console_set_fullscreen(not lcod.console_is_fullscreen())
+
+        if game_state == GameStates.ENEMY_TURN:
+            for entity in entities:
+                if entity != player:
+                    print("The " + entity.name + " wonders if there's anything but spam and eggs")
+
+            game_state = GameStates.PLAYER_TURN
+
 
 if __name__ == "__main__":
     main()
